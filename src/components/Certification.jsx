7704bf58494d06f772a5d3f24366b00d6e22248d
Chosen Esiwe <chosenuche24@gmail.com>
2025-05-28 19:29:36 +0100

    Update: added certifications section

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { MdOutlineArrowOutward } from "react-icons/md";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 import "./Certification.css";
 
 const certifications = [
   {
     title: "Web Development(Frontend)",
     issuer: "HNG Internship Program",
-    date: "November 2024",
+    date: "07th August 2024",
     link: "HNG Internship 11_Nwafor Arinzechukwu_certificate.pdf",
   },
   {
     title: "Web Development(Frontend)",
     issuer: "New Horizon Academy,",
-    date: "July 2024",
+    date: "20th October 2024",
     link: "New Horizon-Nwafor Arinzechukwu Chidera.pdf",
   },
   {
     title: "Word Processing",
     issuer: "Cisco Networking Academy",
-    date: "July 2018",
+    date: "26th October 2018",
     link: "/cisco.pdf",
   },
 ];
@@ -38,7 +38,7 @@ const Certification = () => {
                     <p><strong>Issued by:</strong> {cert.issuer}</p>
                     <p><strong>Date:</strong> {cert.date}</p>
                     <a href={cert.link} target="_blank" rel="noopener noreferrer" className="cert-btn">
-                    View Certificate <span><MdOutlineArrowOutward /></span>
+                    View Certificate <span><BsBoxArrowUpRight /></span>
                     </a>
                 </div>
                 ))}
